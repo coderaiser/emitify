8d24d4ac20d2874d10bd530f2d6f1b813235bedb
coderaiser <mnemonic.enemy@gmail.com>
2015-12-29 13:52:54 -0500

    feature(emitify) global: lib/emitify -> dist/emitify

--- test/emitify.js
+++ test/emitify.js
@@ -1,86 +1,100 @@
-(function() {
-    'use strict';
+'use strict';
+
+let Emitify = require('..'),
+    test    = require('tape');
+
+test('on', t => {
+    let emitify = Emitify();
     
-    let Emitify = require('..'),
-        test    = require('tape');
-    
-    test('on', t => {
-        let emitify = Emitify();
-        
-        emitify.on('event', () => {
-            t.end();
-        });
-        
-        emitify.emit('event');
-    });
-    
-    test('addListener', t => {
-        let emitify = Emitify();
-        
-        emitify.addListener('event', () => {
-            t.end();
-        });
-        
-        emitify.emit('event');
-    });
-    
-    test('once', t => {
-        let emitify = Emitify(),
-            was,
-            fn      = () => {
-                was && t.fail('listener should be off');
-                was = true;
-            };
-        
-        emitify.once('event', fn);
-        
-        emitify.emit('');
-        
+    emitify.on('event', () => {
         t.end();
     });
     
-    test('on error: no listeners', t => {
-        let emitify = Emitify(),
-            fn  = function() {
-                emitify.emit('error', Error('Some error!'));
-            };
-       
-       t.throws(fn, /Some error!/, 'should throw exception');
-       t.end();
-    });
+    emitify.emit('event');
+});
+
+test('addListener', t => {
+    let emitify = Emitify();
     
-    test('off', t => {
-        let emitify = Emitify(),
-            fn      = () => {
-                t.fail('listener should be off');
-            };
-        
-        emitify
-            .on('event', fn)
-            .off('event', fn);
-        
-        emitify.emit('event');
-        
+    emitify.addListener('event', () => {
         t.end();
     });
     
-    test('on, addListener: no arguments', t => {
-        let emitify = Emitify(),
-            fn  = function() {
-                emitify.on();
-            };
-       
-       t.throws(fn, /event should be string!/, 'should throw when not string');
-       t.end();
+    emitify.emit('event');
+});
+
+test('addListener, add second one', t => {
+    let emitify = Emitify();
+    let value   = 0;
+    let inc     = () => ++value;
+    
+    emitify.addListener('run', () => {
+        t.equal(inc(), 1);
     });
     
-    test('on, addListener: no listener', t => {
-        let emitify = Emitify(),
-            fn  = function() {
-                emitify.on('error');
-            };
-       
-       t.throws(fn, /callback should be function!/, 'should throw when not function');
-       t.end();
+    emitify.addListener('run', () => {
+        t.equal(inc(), 2);
+        t.end();
     });
-})();
+    
+    emitify.emit('run');
+});
+
+test('once', t => {
+    let emitify = Emitify(),
+        was,
+        fn      = () => {
+            was && t.fail('listener should be off');
+            was = true;
+        };
+    
+    emitify.once('event', fn);
+    emitify.emit('event');
+    
+    t.end();
+});
+
+test('on error: no listeners', t => {
+    let emitify = Emitify(),
+        fn  = function() {
+            emitify.emit('error', Error('Some error!'));
+        };
+   
+   t.throws(fn, /Some error!/, 'should throw exception');
+   t.end();
+});
+
+test('off', t => {
+    let emitify = Emitify(),
+        fn      = () => {
+            t.fail('listener should be off');
+        };
+    
+    emitify
+        .on('event', fn)
+        .off('event', fn);
+    
+    emitify.emit('event');
+    
+    t.end();
+});
+
+test('on, addListener: no arguments', t => {
+    let emitify = Emitify(),
+        fn  = function() {
+            emitify.on();
+        };
+   
+   t.throws(fn, /event should be string!/, 'should throw when not string');
+   t.end();
+});
+
+test('on, addListener: no listener', t => {
+    let emitify = Emitify(),
+        fn  = function() {
+            emitify.on('error');
+        };
+   
+   t.throws(fn, /callback should be function!/, 'should throw when not function');
+   t.end();
+});
